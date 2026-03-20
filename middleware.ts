4d44763451ce7c3871ed1e804beabe21200e6a83
johnarrackalceles-ui <johnarrackalceles@gmail.com>
2026-03-19 19:48:18 -0700

    Removed clerk protection on public api endpoint and added rate limiting

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,17 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher([
+  "/api/stripe(.*)",
+  "/api/stripeCheckout(.*)",
+  "/billing(.*)",
+  "/enterprise(.*)",
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) {
+    auth().protect();
+  }
+});
 
 export const config = {
   matcher: ["/((?!_next|.*\\..*).*)", "/(api|trpc)(.*)"],
