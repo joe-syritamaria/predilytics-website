a409072c68b8ccdb19746a89c6e02c05195aca94
johnarrackalceles-ui <johnarrackalceles@gmail.com>
2026-06-28 20:15:26 -0700

    Stabilize GEMLA checkout auth flow

--- app/(mainpage)/gemla-zeta/checkout/page.tsx
+++ app/(mainpage)/gemla-zeta/checkout/page.tsx
@@ -1,7 +1,15 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import GemlaCheckoutClient from "./GemlaCheckoutClient";
 
 export const dynamic = "force-dynamic";
 
-export default function GemlaCheckoutPage() {
+export default async function GemlaCheckoutPage() {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/sign-in?redirect_url=/gemla-zeta/checkout");
+  }
+
   return <GemlaCheckoutClient />;
 }
